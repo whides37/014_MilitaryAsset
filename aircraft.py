31716f11c27874c9ea9f5f61cb667ff479d07f9d
whides37 <w.hides37@gmail.com>
2026-02-14 16:13:27 +0900

    itemsを入力

---
+++ aircraft.py
@@ -0,0 +1,10 @@
+class AircraftInventory:
+
+    def __init__(self):
+        self.items = {
+            "A6M Zero": 5,
+            "J7W Shinden": 2,
+            "J1N Gekko": 3,
+            "A7M Reppu": 4
+        }
+
