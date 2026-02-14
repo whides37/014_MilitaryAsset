51ffe691f45906feecb6809ad1562db21ddf7a0b
whides37 <w.hides37@gmail.com>
2026-02-14 16:22:01 +0900

    追加メソッド作成

--- aircraft.py
+++ aircraft.py
@@ -7,4 +7,18 @@ class AircraftInventory:
             "J1N Gekko": 3,
             "A7M Reppu": 4
         }
+    def add_item(self,name, quantity):
+        if not isinstance(name,str):
+            raise TypeError("名前は機種と機種名で入力してください")
+        if not isinstance(quantity,int):
+            raise TypeError("機体数は数値で入力してください")
+        if quantity <= 0 :
+            raise ValueError("機体数は0以上の数で入力してください")
+        #辞書にあるか確認
+        if name in self.items:
+            self.items[name] += quantity
+        #なければ追加
+        else:
+            self.items[name] = quantity
 
+        
