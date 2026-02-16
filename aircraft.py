04f90ad2eee8e4eceeff2055c32a58b55d79bffc
whides37 <w.hides37@gmail.com>
2026-02-16 09:17:33 +0900

    機体名を和名に変更

--- aircraft.py
+++ aircraft.py
@@ -2,10 +2,10 @@ class AircraftInventory:
 
     def __init__(self):
         self.items = {
-            "A6M Zero": 5,
-            "J7W Shinden": 2,
-            "J1N Gekko": 3,
-            "A7M Reppu": 4
+            "A6M 零戦": 5,
+            "J7W 震電": 2,
+            "J1N 月光": 3,
+            "A7M 烈風": 4
         }
     def add_item(self,name, quantity):
         if not isinstance(name,str):
@@ -21,6 +21,7 @@ class AircraftInventory:
             self.items[name] = quantity
 
     def remove_item(self,name, quantity):
+
         if not isinstance(name,str):
             raise TypeError("名前は機種と機種名で入力してください")
         if not isinstance(quantity,int):
