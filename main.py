5bafc8b747e250f311bf8ec57e9646b871765741
whides37 <w.hides37@gmail.com>
2026-02-16 11:34:21 +0900

    try/except追加

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-""" 
-import aircraft                 ←成功する、import Errorはでない。
-inventry = AircraftInventory()  →NameError 
-
-"""
-
 #aircraft.pyからAircraftInventoryクラスをもってくる
 from aircraft import AircraftInventory
 
 inventry = AircraftInventory()
 
-inventry.add_item("A6M 零戦",10)
-inventry.remove_item("J7W 震電",1)
+try:
+    inventry.add_item("A6M 零戦",10)
+    inventry.remove_item("J7W 震電",10)
+except ValueError as e :
+    print("値エラー:",e)
+except KeyError as e :
+    print("キーエラー:",e)
+except TypeError as e :
+    print("型エラー:",e)
 
 print(inventry)
