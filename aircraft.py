51e6f86df266b4ca94d82f64e88dda989b6d4884
whides37 <w.hides37@gmail.com>
2026-02-16 09:24:52 +0900

    コメント追加、行整理

--- aircraft.py
+++ aircraft.py
@@ -7,11 +7,14 @@ class AircraftInventory:
             "J1N 月光": 3,
             "A7M 烈風": 4
         }
+        
     def add_item(self,name, quantity):
+        #型チェック
         if not isinstance(name,str):
             raise TypeError("名前は機種と機種名で入力してください")
         if not isinstance(quantity,int):
             raise TypeError("機体数は数値で入力してください")
+        #値チェック
         if quantity <= 0 :
             raise ValueError("機体数は1以上の数で入力してください")
         # 既に登録済みの機体（キーの値）なら、なければ新規登録
@@ -21,19 +24,24 @@ class AircraftInventory:
             self.items[name] = quantity
 
     def remove_item(self,name, quantity):
-
+        #型チェック
         if not isinstance(name,str):
             raise TypeError("名前は機種と機種名で入力してください")
         if not isinstance(quantity,int):
             raise TypeError("機体数は数値で入力してください")
+        #値チェック
         if quantity <= 0 :
             raise ValueError("機体数は1以上の数で入力してください")
+        #登録しているかチェック
         if name not in self.items:
             raise KeyError("その機体は登録されていません")
+        #在庫チェック
         if quantity > self.items[name]:
             raise ValueError("在庫不足です")
         self.items[name] -= quantity
 
     def get_stock(self, name):
+        #登録がなければKeyError
         return self.items[name]
+    
 
